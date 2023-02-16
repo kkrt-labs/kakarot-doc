1074e6963299ac69e85127a91a5daa302acf88fc
bal7hazar <bal7hazar@proton.me>
2023-02-16 12:26:13 +0100

    🔧 Fix target

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Kakarot',
   tagline: "EVM interpreter written in Cairo",
-  url: 'http://doc.kakarot.org/',
+  url: 'https://doc.kakarot.org/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
