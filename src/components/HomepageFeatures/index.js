a17228a1a134b71d55a85549258871d406782aed
bal7hazar <bal7hazar@proton.me>
2023-02-16 13:20:08 +0100

    📝 Minor update

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -8,7 +8,7 @@ const FeatureList = [
     Svg: require('@site/static/img/logo.svg').default,
     description: (
       <>
-        More details on how to build the solution <a href="https://github.com/sayajin-labs/kakarot#build" target="_blank">here</a> 
+        More details on how to build the solution <a href="https://github.com/sayajin-labs/kakarot#build" target="_blank">here</a>
       </>
     ),
   },
@@ -26,7 +26,7 @@ const FeatureList = [
     Svg: require('@site/static/img/logo.svg').default,
     description: (
       <>
-      More details on how to contribute <a href="https://github.com/sayajin-labs/kakarot/blob/main/docs/CONTRIBUTING.md" target="_blank">here</a>
+      More details on how to become a SSJ <a href="https://github.com/sayajin-labs/kakarot/blob/main/docs/CONTRIBUTING.md" target="_blank">here</a>
       </>
     ),
   },
